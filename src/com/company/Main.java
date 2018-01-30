9cc3fa8b28df8d542f0abe6ac58f711248435c8f
Elanas <elaasinfo@gmail.com>
2018-01-30 14:57:24 +0200

    first commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,15 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println("Iveskite savo zodi ");
+        Scanner sc = new Scanner(System.in);
+        String zodis = sc.nextLine();
+        System.out.println(zodis);
+        System.out.println(zodis.length());
     }
 }
